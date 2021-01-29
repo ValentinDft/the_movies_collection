95e0b6dde65ec26ff5553dfd54d235a0a6b54a3c
Valentin Duffet <49043842+ValentinDft@users.noreply.github.com>
2021-01-29 15:21:34 +0100

    Ajout de la route movie + initialisation .env pour les requetes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
 var express = require('express');
+var request = require("sync-request");
 var router = express.Router();
+
+var REQUEST_MOVIE = process.env.REQUEST_MOVIE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/movies', function(req, res, next) {
+  
+  let requete = request("GET", REQUEST_MOVIE);
+  let resultatRequete = JSON.parse(requete.getBody());
+ 
+  res.json( { resultatRequete });
 });
 
 module.exports = router;
