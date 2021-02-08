dc0b6c660aa6f3ec34259fcef97734c0b1e9ed80
Valentin Duffet <49043842+ValentinDft@users.noreply.github.com>
2021-02-08 20:28:43 +0100

    initialisation de redux

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,14 +4,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PopularMovies from './PopularMovies';
 import PopularTV from './PopularTV';
 
+import onMovie from "./reducer/page.reducer"
+import {Provider} from "react-redux";
+import {createStore, combineReducers} from "redux";
+const store = createStore(combineReducers({onMovie}));
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={PopularMovies}/>
-        <Route path="/popular-tv" exact component={PopularTV}/>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={PopularMovies}/>
+          <Route path="/popular-tv" exact component={PopularTV}/>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
