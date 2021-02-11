870ce12f0c7176fb8ea287f439c20020d16e4032
Valentin Duffet <49043842+ValentinDft@users.noreply.github.com>
2021-02-11 11:27:23 +0100

    titre avec effet néon

--- reactApp/src/components/nav.js
+++ reactApp/src/components/nav.js
@@ -7,12 +7,12 @@ function nav() {
     return(
         
         <Row>   
-        <Col span={7}>
+        <Col span={8}>
           <Animated animationIn="fadeInLeft">
-            <h1 style={{textAlign: "center", color: "white", fontFamily: 'Coda Caption'}}>The Movies Collection</h1>
+            <h1 style={{fontFamily: 'Codystar'}} className="neons"><em>The Movies Collection</em></h1>
           </Animated>
         </Col>
-        <Col span={10} style={{display: "flex", justifyContent: "center"}}>
+        <Col span={8} style={{display: "flex", justifyContent: "center"}}>
         <Animated animationIn="fadeInDown">
           <ul className="menu-bar">
             <Animated animationIn="bounceInDown">
